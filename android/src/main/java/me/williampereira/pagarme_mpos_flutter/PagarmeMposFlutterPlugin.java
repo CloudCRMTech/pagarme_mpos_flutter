61aa59f88632aa66813dd1a6ee91cdc14a194233
Antonio Silva <antonio@pyxpayment.com>
2025-08-27 14:04:42 -0300

    remove unused maven plugin and deprecated registerWith method from PagarmeMposFlutterPlugin

--- android/src/main/java/me/williampereira/pagarme_mpos_flutter/PagarmeMposFlutterPlugin.java
+++ android/src/main/java/me/williampereira/pagarme_mpos_flutter/PagarmeMposFlutterPlugin.java
@@ -18,14 +18,12 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 
-import io.flutter.app.FlutterActivity;
 import io.flutter.embedding.engine.plugins.FlutterPlugin;
 import io.flutter.plugin.common.EventChannel;
 import io.flutter.plugin.common.MethodCall;
 import io.flutter.plugin.common.MethodChannel;
 import io.flutter.plugin.common.MethodChannel.MethodCallHandler;
 import io.flutter.plugin.common.MethodChannel.Result;
-import io.flutter.plugin.common.PluginRegistry.Registrar;
 import me.pagar.mposandroid.EmvApplication;
 import me.pagar.mposandroid.Mpos;
 import me.pagar.mposandroid.MposListener;
@@ -56,25 +54,6 @@ public class PagarmeMposFlutterPlugin implements FlutterPlugin, MethodCallHandle
     channel.setMethodCallHandler(plugin);
 
     final EventChannel eventChannel = new EventChannel(flutterPluginBinding.getFlutterEngine().getDartExecutor(), "mpos_stream");
-    eventChannel.setStreamHandler(plugin);
-  }
-
-  // This static function is optional and equivalent to onAttachedToEngine. It supports the old
-  // pre-Flutter-1.12 Android projects. You are encouraged to continue supporting
-  // plugin registration via this function while apps migrate to use the new Android APIs
-  // post-flutter-1.12 via https://flutter.dev/go/android-project-migration.
-  //
-  // It is encouraged to share logic between onAttachedToEngine and registerWith to keep
-  // them functionally equivalent. Only one of onAttachedToEngine or registerWith will be called
-  // depending on the user's project. onAttachedToEngine or registerWith must both be defined
-  // in the same class.
-  public static void registerWith(Registrar registrar) {
-    PagarmeMposFlutterPlugin plugin = new PagarmeMposFlutterPlugin(registrar.activity().getApplicationContext());
-
-    final MethodChannel channel = new MethodChannel(registrar.messenger(), "pagarme_mpos_flutter");
-    channel.setMethodCallHandler(new PagarmeMposFlutterPlugin(registrar.activity().getApplicationContext()));
-
-    final EventChannel eventChannel = new EventChannel(registrar.messenger(), "mpos_stream");
     eventChannel.setStreamHandler(plugin);
   }
 
